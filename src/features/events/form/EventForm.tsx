2a9ebce0775e94f1078d772db0e10e9d77ade9b6
Muhammadi2 <mshislam@gmail.com>
2024-01-07 08:04:39 -0500

    Lecture 62 re-enabling update and submit

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -4,8 +4,12 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../app/store";
 // import { createEvent, updateEvent } from "../eventSlice";
 // import { createId } from "@paralleldrive/cuid2";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, useForm, FieldValues } from "react-hook-form";
 import { categoryOptions } from "./categoryOptions";
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from 'react-datepicker';
+import { createId } from "@paralleldrive/cuid2";
+import { createEvent, updateEvent } from "../eventSlice";
 // import { createId } from "@paralleldrive/cuid2";
 
 
@@ -21,27 +25,29 @@ export default function EventForm() {
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
-    const initialValues = event ?? {
-        title:'',
-        category:'',
-        description:'',
-        city:'',
-        venue:'',
-        date:''
-    }
+    // const initialValues = event ?? {
+    //     title:'',
+    //     category:'',
+    //     description:'',
+    //     city:'',
+    //     venue:'',
+    //     date:''
+    // }
     // Go to event or Set an Empty values
 
     // const [values, setValues] = useState(initialValues);
 
     function onSubmit(data:FieldValues){
-            console.log(data);
-        // id = id ?? createId()
-        // event
-        //     ? dispatch(updateEvent({...event, ...values}))
-        //     // : dispatch(createEvent({...values, id:createId(), hostedBy:'', attendees: [],hostPhotoURL: ''})) problem with createId
-        //     : dispatch(createEvent({...values, id, hostedBy:'', attendees: [],hostPhotoURL: ''}))
-        // navigate(`/events/${id}`);
-        // console.log(values)
+            // console.log(data);
+        id = id ?? createId()
+        event
+            // ? dispatch(updateEvent({...event, ...values}))
+            // // : dispatch(createEvent({...values, id:createId(), hostedBy:'', attendees: [],hostPhotoURL: ''})) problem with createId
+            // : dispatch(createEvent({...values, id, hostedBy:'', attendees: [],hostPhotoURL: ''}))
+            ? dispatch(updateEvent({ ...event, ...data, date: data.date.toString()}))
+            : dispatch(createEvent({ ...data, id, hostedBy: 'bob', attendees: [], hostPhotoURL: '', date: data.date.toString()}))
+        navigate(`/events/${id}`);
+       
         
     }
 
@@ -105,20 +111,42 @@ export default function EventForm() {
                 {...register('venue', {required: 'Venue  is required'})}
                 error = {errors.venue && errors.venue.message}
                 />
-
-            <Form.Input
+            <Form.Field>
+                <Controller 
+                    name='date'
+                    control={control}
+                    rules ={{required:'Date is required'}}
+                    defaultValue={event && new Date(event.date) || null}
+                    render={({field}) => (
+                        <DatePicker 
+                            selected={field.value}
+                            onChange={value => setValue('date', value, {shouldValidate: true})}
+                            showTimeSelect
+                            timeCaption='time'
+                            dateFormat='MMM d, yyyy h:mm aa'
+                            placeholderText='Event date and time'
+                            />
+                            )}
+                />
+            </Form.Field>
+            {/* <Form.Input
                 type= 'date'
                 placeholder='Date'
                 defaultValue={event?.date || ''}
                 {...register('date', {required:'Date is required'})}
                 error = {errors.date && errors.date.message}
                 />
-        
-            <Button 
+         */}
+            {/* <Button 
             loading={isSubmitting}
             disabled={!isValid}
             type='submit' floated='right' positive content='Submit' />
-            <Button disabled={isSubmitting} as={Link} to='/events' type='button' floated='right'  content='Cancel' />
+            <Button disabled={isSubmitting} as={Link} to='/events' type='button' floated='right'  content='Cancel' /> */}
+             <Button
+                    loading={isSubmitting}
+                    disabled={!isValid}
+                    type='submit' floated='right' positive content='Submit' />
+                <Button disabled={isSubmitting} as={Link} to='/events' type='button' floated='right' content='Cancel' />
         </Form>
 
     </Segment>
