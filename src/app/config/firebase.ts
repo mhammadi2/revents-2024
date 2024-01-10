5669a2684c39b6127beb2c2874c3a0f8ec7e960b
Muhammadi2 <mshislam@gmail.com>
2024-01-09 19:43:33 -0500

    Lecture 87 loggin  usinh firebase authetication and user

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
+import 'firebase/auth';
 import 'firebase/firestore';
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,3 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -3,23 +3,43 @@ import { Button, Form } from 'semantic-ui-react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { useAppDispatch } from '../../app/store/store';
 import ModalWrapper from '../../app/common /modals/ModalWrapper';
-import { closeModal } from '../../app/common /modals/modalSlice';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../app/config/firebase';
 import { signIn } from './authSlice';
+import { closeModal } from '../../app/common /modals/modalSlice';
 // import { closeModal } from '../../app/common/modals/modalSlice';
 // import { signIn } from './authSlice';
 
 export default function LoginForm() {
-    const {register, handleSubmit, formState: {isSubmitting, isValid, isDirty, errors}} = useForm({
+    // const {register, handleSubmit, formState: {isSubmitting, isValid, isDirty, errors}} = useForm({
+    //     mode: 'onTouched'
+    // })
+
+    
+    const { register, handleSubmit, formState: { isSubmitting, isValid, isDirty, errors } } = useForm({
         mode: 'onTouched'
     })
     const dispatch = useAppDispatch();
 
-    function onSubmit(data: FieldValues) {
-        // console.log(data);
-        dispatch(signIn(data))
-        dispatch(closeModal());
+    // function onSubmit(data: FieldValues) {
+    //     dispatch(signIn(data))
+    //     dispatch(closeModal());
+    // }
+    async function onSubmit(data: FieldValues) {
+        try {
+            const result = await signInWithEmailAndPassword(auth, data.email, data.password);
+            console.log(result);
+            dispatch(signIn(result.user));
+            dispatch(closeModal());
+            // await signInWithEmailAndPassword(auth, data.email, data.password);
+            // dispatch(closeModal());
+        } catch (error) {
+            console.log(error)
+            // setError('root.serverError', {
+            //     type: '400', message: error.message
+            // })
+        }
     }
-
     return (
         <ModalWrapper header='Sign into re-vents'>
             <Form onSubmit={handleSubmit(onSubmit)}>
